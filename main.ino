c53164124d2766f46d66f1ece452a8e833cc3fb7
Michael Czigler <37268479+mczigler@users.noreply.github.com>
2020-01-28 20:43:55 -0500

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,11 +1,17 @@
-// the unstoppable billy bass!
+/*
+    Billy Bass in Motion
+    Author: Michael Czigler
+    Version: 1.0
+*/
+
+
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
-Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(2);
+Adafruit_DCMotor *mouth_motor = AFMS.getMotor(1);
+Adafruit_DCMotor *tail_motor = AFMS.getMotor(2);
 
 int SoundInPin = A0;
 int LedPin =12;
@@ -13,15 +19,17 @@ int LedPin =12;
 void setup() {
     Serial.begin(9600);
     AFMS.begin();
-    myMotor->setSpeed(0);
-    myMotor->run(FORWARD);      // mouth motor
-    myMotor->run(RELEASE);      // turn on motor
-    pinMode(SoundInPin, INPUT);
-    pinMode(LedPin, OUTPUT);
-    myOtherMotor->setSpeed(0);
-    myOtherMotor->run(FORWARD); //tail motor
-    myOtherMotor->run(RELEASE);
-    pinMode(SoundInPin, INPUT);
+
+    mouth_motor->setSpeed(0);
+    mouth_motor->run(FORWARD);  // turn on motor
+    mouth_motor->run(RELEASE);  // release motor
+
+    tail_motor->setSpeed(0);
+    tail_motor->run(FORWARD);   // turn on motor
+    tail_motor->run(RELEASE);   // release motor
+
+    pinMode(SoundInPin, INPUT); // set audio pin mode
+    pinMode(LedPin, OUTPUT);    // set led pin mode
 }
 
 void loop() {
@@ -29,18 +37,40 @@ void loop() {
     int sensorValue = analogRead(SoundInPin);
     int LEDValue = map(sensorValue,0,512,0,255);
     sensorValue = map(sensorValue,0,512,0,180);
+
     int MoveDelayValue = map(sensorValue,0,255,0,sensorValue);
 
-    if (sensorValue > 10) {
+//    if (sensorValue > 10) {
+//        delay(1);
+//        mouth_motor->run(FORWARD);
+//        for (i=140; i<255; i++) {
+//            mouth_motor->setSpeed(i);
+//        }
+//        analogWrite(LedPin, sensorValue);
+//        mouth_motor->run(RELEASE);
+//        tail_motor->run(RELEASE);
+//        delay(1);
+//    }
+    // 
+    if (sensorValue > 13) {
         delay(1);
-        myMotor->run(FORWARD);
-        for (i=140; i<255; i++) {
-            myMotor->setSpeed(i);
+        head_motor->setSpeed(200);
+        delay(1)
+        head_motor->run(FORWARD);
+        delay(2000);
+    }
+
+    if (sensorValue > 12) {
+        delay(1);
+        mouth_motor->run(FORWARD);
+        for (i=140; i<255 i++) {
+            mouth_motor->setSpeed(i);
         }
         analogWrite(LedPin, sensorValue);
-        myMotor->run(RELEASE);
-        myOtherMotor->run(RELEASE);
+        mouth_motor->run(RELEASE);
+        tail_motor->run(RELEASE)
         delay(1);
     }
+
     analogWrite(LedPin, 0);
 }
